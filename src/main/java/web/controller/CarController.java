49ab8d39e1318d2f8fc463184ea9e96832859b42
Dmitry Mishakov <welzevul0@gmail.com>
2022-04-08 12:55:57 +0300

    Created normal Request Parameter of count cars

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,9 +3,7 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import web.services.CarServices;
 
 @Controller
@@ -19,22 +17,15 @@ public class CarController {
         this.carServices = carServices;
     }
 
+
     @GetMapping()
-    public String allCars(Model model) {
-        carServices.createListOfCars(1);
-        model.addAttribute("cars", carServices.listCars(0));
-        return "cars";
-    }
-
-    @PostMapping()
-    public String createDefaultCars() {
-        carServices.createListOfCars(7);
-        return "redirect:/cars";
-    }
-
-    @GetMapping("/head1")
-    public String getCountCar() {
-        return "cars";
+    public String getCountCar(@RequestParam(value = "count", required = false) Integer i,
+                              Model model) {
+        if (i == null) {
+            i = 0;
+        }
+        model.addAttribute("cars", carServices.listCars(i));
+        return "/cars";
     }
 
 }
