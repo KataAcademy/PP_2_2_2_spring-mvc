3ef4359e288a073d3fe6260259882c122ff2c031
spasiandsohrany <kazakov.nikitta@yandex.ru>
2023-02-10 20:07:16 +0300

    2

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -9,7 +9,7 @@ import web.service.CarService;
 @Controller
 public class CarsController {
 
-    private final CarService carService;
+    private CarService carService;
 
     public CarsController(CarService carService) {
         this.carService = carService;
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,30 +1,12 @@
 package web.service;
 
-import org.springframework.stereotype.Service;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
-@Service
-public class CarService {
 
-    private List<Car> cars = new ArrayList<>();
+public interface CarService {
 
-    public CarService() {
-        cars.add(new Car("Toyota", "Camry", 2020));
-        cars.add(new Car("Honda", "Civic", 2021));
-        cars.add(new Car("Tesla", "Model 3", 2022));
-        cars.add(new Car("Ford", "Mustang", 2019));
-        cars.add(new Car("Chevrolet", "Corvette", 2018));
-    }
+    List<Car> getCars();
 
-    public List<Car> getCars() {
-        return cars;
-    }
-
-    public List<Car> getCars(int count) {
-        return cars.subList(0,count);
-    }
+    List<Car> getCars(int count);
 }
-
-
