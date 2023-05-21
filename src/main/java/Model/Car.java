337eaa8c5b1ceb70c8df224b5717a763196dbded
Chilloutsaur <110351523+Chilloutsaur@users.noreply.github.com>
2023-05-21 21:35:59 +0300

    Ошибка 404 при запуске.

---
+++ src/main/java/Model/Car.java
@@ -0,0 +1,42 @@
+package Model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+    private String model;
+    private String color;
+    private int serial;
+
+    public Car(String model, String color, int serial) {
+        this.model = model;
+        this.color = color;
+        this.serial = serial;
+    }
+
+    public Car() {
+    }
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSerial() {
+        return serial;
+    }
+
+    public void setSerial(int serial) {
+        this.serial = serial;
+    }
+}
---
+++ src/main/java/web/controller/CarsController.java
@@ -0,0 +1,27 @@
+package web.controller;
+
+import Model.Car;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+
+public class CarsController {
+
+    @GetMapping("/cars")
+    public  String getCars(ModelMap model) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Model1","Color1", 1));
+        cars.add(new Car("Model2","Color2", 2));
+        cars.add(new Car("Model3","Color3", 3));
+        cars.add(new Car("Model4","Color4", 4));
+        cars.add(new Car("Model5","Color5", 5));
+        model.addAttribute("cars", cars);
+        return "car";
+    }
+}
